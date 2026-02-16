fa0069a9a37c388bfdfcb0ea7f1abb24dfa0fd27
Scott Dalpias <instructionalmotion@gmail.com>
2026-02-16 13:14:13 -0600

    Add Header component and integrate it into the App layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import Header from './components/ui/Header';
+
 export default function App() {
   return (
     <div className="flex min-h-screen items-start justify-center bg-gray-100">
-      <div className="flex w-full flex-col bg-amber-200 md:max-w-2xl">
+      <div className="flex w-full flex-col md:max-w-2xl">
+        <Header />
       </div>
     </div>
   );
